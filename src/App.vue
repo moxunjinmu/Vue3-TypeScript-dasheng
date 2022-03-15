0e0f0733587084fac775d8423594897a166bca5f
moxunjingmu <1786471868@qq.com>
2022-03-15 23:03:33 +0800

    feat：添加todo功能

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template >
   <div>
-    
+    <input type="text" v-model="val">
+    <button @click="addTodo">添加</button>
+    <ul>
+      <li v-for="items in todos" >{{items}}</li>
+    </ul>
   </div>
 </template>
-<script lang="ts">
-import { ref } from "vue"
-export default{
-  setup(){
-    let count
-    return
-  }
+<script lang="ts" setup>
+import { reactive, ref } from "vue"
+ 
+let val = ref('')
+let todos = reactive(['吃饭', '睡觉', '学习vue3'])
+
+function addTodo() {
+  todos.push(val.value)
+  val.value = ''
 }
+
 </script>
 <style>
 </style>
