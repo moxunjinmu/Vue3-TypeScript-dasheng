3828d2b6ba142855295626056a42e5135374d787
moxunjingmu <1786471868@qq.com>
2022-03-17 01:02:57 +0800

    feat：完成部分评分功能，疑问em为啥与font-size不相等

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,11 @@
-<template >
-  <div>
-    <h1>{{mousex}}:{{y}}</h1>
-    <input type="text" v-model="val" />
-    <button @click="addTodo">添加</button>
-    <button @click="clearTodo">清理</button>
-    <ul v-if="todos.length">
-      <li v-for="(todo,i) in todos" :key="i">
-        <input type="checkbox" v-model="todo.done">
-        <span :class="{done: todo.done}">{{ todo.title }}</span>
-        <span @click="removeTodo(i)">❌</span>
-        </li>
-    </ul>
-    <div v-else>暂无数据</div>
-    全选：<input type="checkbox" v-model="allDone" />
-    <span>{{active}}/{{all}}</span>
-  </div>
+<template>
+ <div>
+  <WRate :value="score" color="red"/>
+  <WRate :value="score" color="#ccc"/>
+ </div>
 </template>
 <script lang="ts" setup>
-import { computed, onMounted, onUnmounted, reactive, Ref, ref } from "vue";
-import useMouse from './composiable/mouse'
-
-let {x:mousex,y} = useMouse()
-const x = ref(1)
-
-interface Todo {
-  title: string,
-  done:boolean
-}
-// let val = ref<string>("");
-let val:Ref = ref('')
-
-let todos = ref<Todo[]>([
-  { title: "吃饭", done: true },
-  { title: "睡觉", done: false },
-  { title: "学习", done: false }
-]);
-
-function addTodo() {
-  todos.value.push({title: val.value, done: false});
-  val.value = "";
-}
-
-
-let active = computed<number>(() =>todos.value.filter(v=>v.done).length)
-let all = computed<number>(()=>todos.value.length)
-let allDone = computed<boolean>({
-  get(){
-    return active.value === all.value
-  },
-  set(value:boolean){
-    todos.value.forEach(todo=>{
-      todo.done = value
-    })
-  }
-})
-
-function removeTodo(i:number) {
-  todos.value.splice(i,1)
-}
-
-function clearTodo() {
-  // 这里使用reactive时不能直接赋值，因为定义的是proxy代理对象
-  todos.value = todos.value.filter(todo=>!todo.done)
-}
-
+import { ref } from 'vue';
+import WRate from './components/WRate.vue';
+let score = ref(3)
 </script>
-<style>
-span.done{
-  text-decoration: line-through;
-  color: rgb(182, 118, 21);
-}
-</style>
---
+++ src/components/WRate.vue
@@ -0,0 +1,44 @@
+
+<template>
+  <!-- <div>
+    <h2 :style="{color: color}">{{score}}</h2>
+  </div> -->
+  <div :style="{ color: color }">
+    <div class="rate">
+      <span v-for="num in 5" :key="num">☆</span>
+      <span class="hollow" :style="starWidth">
+        <span v-for="num in 5" :key="num">★</span>
+      </span>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { computed } from "vue";
+
+let props = defineProps<{
+  value: number;
+  color: string;
+}>();
+
+let score = computed(() => {
+  // slice选取从开始到结尾返回一个数组
+  return "★★★★★☆☆☆☆☆".slice(5 - props.value, 10 - props.value);
+});
+let color = computed(() => props.color);
+let starWidth = computed(() => `width:${props.value}em;`)
+</script>
+<style scoped>
+
+.rate {
+  position: relative;
+  display: inline-block;
+}
+.rate > .hollow {
+  position: absolute;
+  display: inline-block;
+  left: 0;
+  top: 0;
+  /* width: 1em; */
+  overflow: hidden;
+}
+</style>
